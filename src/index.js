9825c66fa4c919a75f41e084e2f3bd5f9706dc8b
Mateus Henrique <mateusdzn10@gmail.com>
2022-06-09 08:10:23 -0400

    feat(database): Connection database mongodb

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 require('dotenv').config();
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 
 const router = require('./routes/routes');
+
+mongoose.Promise = global.Promise;
+
+mongoose.connect(process.env.DATABASE_URL, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+});
 
 app.use('/user', router);
 
