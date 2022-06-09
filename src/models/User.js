62e2ce372bc2c81da88986f551723b822ddbd464
Mateus Henrique <mateusdzn10@gmail.com>
2022-06-09 08:13:38 -0400

    feat: create model user

---
+++ src/models/User.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+// Declare the Schema of the Mongo model
+const userSchema = new mongoose.Schema({
+	name: {
+		type: String,
+		required: true,
+		unique: true,
+		index: true,
+	},
+	email: {
+		type: String,
+		required: true,
+		unique: true,
+	},
+
+	password: {
+		type: String,
+		required: true,
+	},
+});
+
+module.exports = mongoose.model('User', userSchema);
