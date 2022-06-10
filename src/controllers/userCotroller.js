f3eaff8e9fe6add3fec5b5ae8cc088c679489a9a
Mateus Henrique <mateusdzn10@gmail.com>
2022-06-09 22:07:05 -0400

    feat: input validation with JOI

--- src/controllers/userCotroller.js
+++ src/controllers/userCotroller.js
@@ -1,8 +1,15 @@
 const User = require('../models/User');
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcryptjs/dist/bcrypt');
+const joiValidate = require('./validate');
 
 const register = async (req, res) => {
+	const { error } = joiValidate.registerValidate(req.body);
+
+	if (error) {
+		return res.status(400).send(error);
+	}
+
 	const selectedUser = await User.findOne({ email: req.body.email });
 
 	if (selectedUser) {
@@ -19,11 +26,17 @@ const register = async (req, res) => {
 		const userSalved = await user.save();
 		res.send(userSalved);
 	} catch (error) {
-		res.status(400).send(error.message);
+		res.status(400).send(error);
 	}
 };
 
 const login = async (req, res) => {
+	const { error } = joiValidate.loginValidate(req.body);
+
+	if (error) {
+		return res.status(400).send(error.message);
+	}
+
 	const selectedUser = await User.findOne({ email: req.body.email });
 	if (!selectedUser) {
 		return res.status(400).send('Email or password incorrect');
